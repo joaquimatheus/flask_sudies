4d3ecaa861f84e8f17b292097ff6fcbecd540f80
joamatheus <joaquimatheus1@gmail.com>
2023-04-07 15:30:35 -0300

    added form validate and login and login validate_on_submit

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,15 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-with app.app_context():
-    db.create_all()
-
-
 
 from app import routes, models
